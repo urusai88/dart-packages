1a0f35dd58fac204fa2daa339ee55492c365c1fc
urusai88 <archon.saratov@gmail.com>
2023-12-23 02:14:43 +0400

    useAsyncCall

--- packages/mhc_flutter/lib/src/packages/hooks/call.dart
+++ packages/mhc_flutter/lib/src/packages/hooks/call.dart
@@ -6,26 +6,34 @@ import 'typedefs.dart';
 typedef CallFunction = dynamic Function();
 
 void useCall(CallFunction function, [HookKeys keys]) =>
-    _CallHook(keys: keys, function: function);
+    use(_CallHook(keys: keys, function: function, sync: true));
 
-class _CallHook extends Hook<void> {
-  const _CallHook({super.keys, required this.function});
+void useAsyncCall(CallFunction function, [HookKeys keys]) =>
+    use(_CallHook(keys: keys, function: function, sync: false));
+
+class _CallHook extends Hook<dynamic> {
+  const _CallHook({super.keys, required this.function, required this.sync});
 
   final CallFunction function;
+  final bool sync;
 
   @override
   _CallHookState createState() => _CallHookState();
 }
 
-class _CallHookState extends HookState<void, _CallHook> {
+class _CallHookState extends HookState<dynamic, _CallHook> {
   @override
   void initHook() {
     super.initHook();
-    hook.function.call();
+    if (hook.sync) {
+      hook.function.call();
+    } else {
+      Future(hook.function.call);
+    }
   }
 
   @override
-  void build(BuildContext context) {}
+  dynamic build(BuildContext context) {}
 
   @override
   String? get debugLabel => 'useCall';
